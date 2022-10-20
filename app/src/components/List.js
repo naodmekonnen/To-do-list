12c670a41027053a10990b0291add669fedb1d7e
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-10-20 21:03:04 +0000

    what now

--- app/src/components/List.js
+++ app/src/components/List.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-const TodoList = ({text, todo, setTodo, task, handleIncrement, handleDecrement})=>{
+const TodoList = ({text, todo, setTodo, task})=>{
  
 const deleteHandler = () =>{
     setTodo(todo.filter((item) => item.id !== task.id))
      
 }
 
+// object literals
 const completedHandler = () => {
     setTodo(todo.map(item => {
         if(item.id === task.id){
-    return {...item, completed: !item.completed
+    return {...item, completed: !item.completed 
     }
         }
         return item;
+        
     }))
 }
 
@@ -35,4 +37,3 @@ return(
 
 export default TodoList
 
-{/* <button onClick ={completedHandler}>done</button> */}
